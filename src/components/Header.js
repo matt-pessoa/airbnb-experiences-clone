9fcc06c68cc30c3e2ec0f7c86aba50190cf90c8e
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-01-13 19:58:49 -0300

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import airbnbLogo from "../images/airbnb-logo.png";
+
+function Header() {
+	return <img src={airbnbLogo} alt="AirBnb logo" />;
+}
+
+export default Header;
