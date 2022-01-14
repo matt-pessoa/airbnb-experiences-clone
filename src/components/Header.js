3b14dddc7aecd647d3eade91b67f30e82246ea7e
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-01-13 22:09:42 -0300

    stylize navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
 import airbnbLogo from "../images/airbnb-logo.png";
+import "./css/Header.css";
 
 function Header() {
-	return <img src={airbnbLogo} alt="AirBnb logo" />;
+	return (
+		<header>
+			<img src={airbnbLogo} alt="AirBnb logo" />
+		</header>
+	);
 }
 
 export default Header;
